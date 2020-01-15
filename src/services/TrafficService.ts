032682df11df6e1546e5aca90b91ed3d420c66ee
Henrik von Hofmeister <hh@cloudscrape.com>
2020-01-15 16:20:52 +0100

    Moved traffic to types

--- src/services/TrafficService.ts
+++ src/services/TrafficService.ts
@@ -1,24 +1,11 @@
-
 import SocketService from "./SocketService";
-
-import { HTTPResponse, HTTPRequest } from "@blockware/ui-web-types";
+import {Traffic} from "@blockware/ui-web-types";
 
 export enum TrafficEventType {
     TRAFFIC_START = 'traffic_start',
     TRAFFIC_END = 'traffic_end'
 }
 
-export interface Traffic {
-    connectionId: string
-    consumerMethodId: string
-    created: number
-    ended: number
-    error: string
-    id: string
-    providerMethodId:string
-    request:HTTPRequest
-    response?:HTTPResponse
-}
 
 export type ConnectionListener = (traffic:Traffic) => void
 
