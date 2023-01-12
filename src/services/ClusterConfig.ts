4604008891aeb13910ebe099f7bed8956c1aa875
Henrik Hofmeister <henrik@newdawn.dk>
2023-01-12 23:57:15 +0100

    Protect against using trying to access process when it's not there

--- src/services/ClusterConfig.ts
+++ src/services/ClusterConfig.ts
@@ -6,11 +6,16 @@ let baseUrl;
 
 if (isElectron()) {
     //We're inside Electron
-
     const localClusterConfig = electronRemote('@blockware/local-cluster-config');
     baseUrl = localClusterConfig.getClusterServiceAddress();
-} else {
+} else if (typeof process !== 'undefined' && process.env) {
+    //We're inside Node
     baseUrl = process.env.REACT_APP_CLUSTER_SERVICE;
+} else if (typeof window !== 'undefined' &&
+    window['Blockware'] &&
+    window['Blockware']['config']) {
+    //We're inside a browser
+    baseUrl = window['Blockware']['config'].cluster_service;
 }
 
 export const CLUSTER_SERVICE_BASEURL = baseUrl ? baseUrl : "http://localhost:35100";
