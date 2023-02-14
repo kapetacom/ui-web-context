cd1562a31504eb1a0ef74106bb69ac21766b3995
Jacob Wejendorp <jacob@wejendorp.dk>
2023-02-14 13:40:49 +0100

    feat: add IdentityService to resolve memberships

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './ResourceTypeProvider';
 export * from './services/AssetService';
 export * from './services/BlockService';
 export * from './services/FileSystemService';
+export * from './services/IdentityService';
 export * from './services/InstanceService';
 export * from './services/PlannerService';
 export * from './services/SocketService';
