01b79ad377ba2322ba0ab65d0b1cf1562cdf9e72
Henrik Hofmeister <henrik@newdawn.dk>
2023-04-21 09:45:34 +0200

    Also export utils

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,4 @@ export * from './services/InstanceService';
 export * from './services/PlannerService';
 export * from './services/SocketService';
 export * from './services/TrafficService';
+export * from './utils';
