5b0be9c3491c1886a29dc0318bfca5799a7c11e9
Henrik Hofmeister <henrik@newdawn.dk>
2023-09-03 12:23:20 +0200

    feat: Removed all services from context
    Only types remain - services moved to desktop app

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,5 @@ export * from './BlockTargetProvider';
 export * from './BlockTypeProvider';
 export * from './ResourceTypeProvider';
 
-export * from './services/ClusterConfig';
-export * from './services/AssetService';
-export * from './services/BlockService';
-export * from './services/FileSystemService';
-export * from './services/IdentityService';
-export * from './services/InstanceService';
-export * from './services/PlannerService';
-export * from './services/SocketService';
-export * from './services/TrafficService';
-export * from './services/TaskService';
+export * from './types';
 export * from './utils';
